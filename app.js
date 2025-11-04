84ffb91d07a7fbbad4b38c2c9873b3feaf6ba820
noumansharif27 <noumansharifgul27@gmail.com>
2025-11-04 17:01:55 +0500

    working on the UI

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
+const path = require("path");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const ejsMate = require("ejs-mate");
 
-app.listen(prompt, () => {
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.engine("ejs", ejsMate);
+
+// Routes
+const listingRoute = require("./routes/index.js");
+
+const PORT = process.env.APP_PORT || 8080;
+
+app.use("/", listingRoute);
+
+app.listen(PORT, () => {
   console.log(`App is listening at ${PORT}`);
 });
---
+++ controllers/index.js
@@ -0,0 +1,3 @@
+module.exports.indexRoute = (req, res) => {
+  res.render("index.ejs");
+};
