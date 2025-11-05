7c76f013efb11e630813e9b79a875f1501795b89
noumansharif27 <noumansharifgul27@gmail.com>
2025-11-05 15:59:26 +0500

    working on the UI

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,15 @@
 module.exports.indexRoute = (req, res) => {
   res.render("index.ejs");
 };
+
+module.exports.smokyOliveRoute = (req, res) => {
+  res.render("landing1.ejs");
+};
+
+module.exports.peacockRoute = (req, res) => {
+  res.render("landing2.ejs");
+};
+
+module.exports.oliveSmokeRoute = (req, res) => {
+  res.render("landing3.ejs");
+};
